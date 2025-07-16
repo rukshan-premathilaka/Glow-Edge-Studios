17199e0579dcca2237214e9520d3f97379d062f0
rukshan-prog <rukshanprog@gmail.com>
2025-07-16 15:17:37 +0530

    add 404.php, home.php

---
+++ views/404.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>404 Error</title>
+    <link rel="stylesheet" href="/css/main.css">
+</head>
+<body>
+
+    <div class="screen-size">
+        <div class="center-around">
+            <h2>404 Error</h2>
+            <h1>Page Not Found!</h1>
+        </div>
+    </div>
+
+
+</body>
+</html>
---
+++ views/home.php
@@ -0,0 +1,3 @@
+<?php
+
+echo "<h1>Welcome to Home Page</h1>";
